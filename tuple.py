a041d44916b6b9c7f02b5b979efe9305c26c4f73
juhoon212 <ws5501@naver.com>
2023-11-10 15:36:37 +0900

    tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+menu = ("돈까스", "치즈까스") #  튜플은 변경, 추가 불가
+print(menu[0])
+print(menu[1])
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+
