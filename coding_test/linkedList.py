058b55b1241042c5d49343d0264894a1c58c6907
juhoon212 <ws5501@naver.com>
2023-11-12 09:27:08 +0900

    기본값

--- coding_test/linkedList.py
+++ coding_test/linkedList.py
@@ -10,4 +10,27 @@ third = Node(3)
 first.next = second
 second.next = third
 first.value = 6
+
+class LinkedList(object):
+    def __init__(self) :
+        self.head = None
+    def append(self, value):
+        new_node = Node(value)
+        
+        if self.head is None:
+            self.head = new_node
+            # 맨 뒤의 node가 new_node를 가리켜야 한다.
+        else : 
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = new_node
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+      
         
