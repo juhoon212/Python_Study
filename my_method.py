f461f21b8b56154c65cbca4dbac78bce72ab354d
juhoon212 <ws5501@naver.com>
2023-11-16 11:31:49 +0900

    상속

--- my_method.py
+++ my_method.py
@@ -1,20 +1,22 @@
-# class Unit:
-#     def __init__(self, name, hp, damage):
-#         self.name = name
-#         self.hp = hp
-#         self.damage = damage
-#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
-#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
-
-class AttackUnit:
-    def __init__(self, name, hp, damage):
+class Unit:
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다.[속도 {2}]"\
+            .format(self.name, location, self.speed))
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
     
     def attack(self, location):
         print("{0} : {1} 방향으로 적군을 공격합닏다. [공격력 {2}]"\
-              .format(self.name, location, self.damage))
+            .format(self.name, location, self.damage))
         
     def damaged(self, damage):
         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
@@ -23,9 +25,42 @@ class AttackUnit:
         if self.hp <= 0:
             print("{0} : 팍괴되었습니다.".format(self.name))
 
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
 
-#공격 2번 받음
-firebat1.damaged(25)
-firebat1.damaged(25)
+# #공격 2번 받음
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+#날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+        
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
+        Flyable.__init__(self, flying_speed)
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+#발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음.
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시 방향")
+battlecruiser.move("9시")
+
+
